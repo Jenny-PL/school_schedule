255f6c672edd8d90559f777320f87b0f8ceb33c2
Jenny <jfperham@gmail.com>
2022-04-27 23:16:50 -0700

    oop_practice from virtual class

--- school_schedule/student.py
+++ school_schedule/student.py
@@ -1,3 +1,10 @@
+# stopped lesson at 33mins.
+# https://adaacademy.hosted.panopto.com/Panopto/Pages/Viewer.aspx?id=6269b2ce-078b-4b7a-b915-ae6a0133fe8f
 class Student:
-    pass
-    
+    def__init__(self, name, year, course_list):
+        self.name = name
+        self.year = year
+        self.course_list = course_list
+        # self.course_list = course_list.copy()    #shallow copy
+        # self.course_list = course_list[:]     #shallow copy
+        #self.course_list = copy.deepcopy  # if use import copy 
